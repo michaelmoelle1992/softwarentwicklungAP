315738fe84ecc4cc1b404e0387b2488657c95a29
MichaelMoelle <michaelmoelle@outlook.de>
2020-05-17 16:25:41 +0200

    Erweiterung der Klassen.h-Datei

--- AP_Vorbereitung_Verkettete_Liste_DYN/klassen.h
+++ AP_Vorbereitung_Verkettete_Liste_DYN/klassen.h
@@ -4,15 +4,25 @@
 #include<stdlib.h>
 #include<cstdio>
 #include<cmath>
+#include<string>
+
+using namespace std;
+
+//Angestellentenverwaltungsprogramm für Zeitarbeitsunternehmen
+//Begründung für dynamische Verwaltung: häufiger Personalaustausch
 
 class Personenelement
 {
 private:
+    string vorname;
+    string nachname;
+    string geburtstag;
 
 protected:
 
 public:
     Personenelement();
+    ~Personenelement();
 };
 
 #endif // KLASSEN_H
